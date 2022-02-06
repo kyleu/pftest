57b5ff9a1461e8e8aad9a62afa1521364779daaf
Kyle Unverferth <kyle@kyleu.com>
2022-02-06 13:16:15 -0700

    .

---
+++ app/lib/schema/model/models.go
@@ -0,0 +1,33 @@
+// Content managed by Project Forge, see [projectforge.md] for details.
+package model
+
+import (
+	"sort"
+
+	"github.com/kyleu/pftest/app/util"
+)
+
+type Models []*Model
+
+func (m Models) Get(pkg util.Pkg, key string) *Model {
+	for _, x := range m {
+		if x.Pkg.Equals(pkg) && x.Key == key {
+			return x
+		}
+	}
+	return nil
+}
+
+func (m Models) Sort() {
+	sort.Slice(m, func(l int, r int) bool {
+		return m[l].Key < m[r].Key
+	})
+}
+
+func (m Models) Names() []string {
+	ret := make([]string, 0, len(m))
+	for _, md := range m {
+		ret = append(ret, md.Key)
+	}
+	return ret
+}
