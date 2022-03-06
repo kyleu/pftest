2541f6b2c0c8e1b8ce6000970830ce7c5b308ff0
Kyle Unverferth <kyle@kyleu.com>
2022-03-06 10:44:32 -0700

    .

--- app/site/menu.go
+++ app/site/menu.go
@@ -2,6 +2,8 @@
 package site
 
 import (
+	"context"
+
 	"github.com/kyleu/pftest/app"
 	"github.com/kyleu/pftest/app/lib/menu"
 	"github.com/kyleu/pftest/app/lib/user"
@@ -10,17 +12,15 @@ import (
 const (
 	keyInstall     = "install"
 	keyDownload    = "download"
-	keyQuickStart  = "quickstart"
 	keyContrib     = "contributing"
 	keyCustomizing = "customizing"
 	keyTech        = "technology"
 )
 
-func Menu(as *app.State, _ *user.Profile, _ user.Accounts) menu.Items {
+func Menu(ctx context.Context, as *app.State, _ *user.Profile, _ user.Accounts) menu.Items {
 	return menu.Items{
 		{Key: keyInstall, Title: "Install", Icon: "code", Route: "/" + keyInstall},
 		{Key: keyDownload, Title: "Download", Icon: "download", Route: "/" + keyDownload},
-		{Key: keyQuickStart, Title: "Quick Start", Icon: "bolt", Route: "/" + keyQuickStart},
 		{Key: keyCustomizing, Title: "Customizing", Icon: "code", Route: "/" + keyCustomizing},
 		{Key: keyContrib, Title: "Contributing", Icon: "cog", Route: "/" + keyContrib},
 		{Key: keyTech, Title: "Technology", Icon: "cog", Route: "/" + keyTech},
