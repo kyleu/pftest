fbe7be64298494ff3d4e7451efcaad2e00756664
Kyle Unverferth <kyle@kyleu.com>
2023-03-05 11:18:26 -0700

    .

--- app/lib/database/sqlite-no.go
+++ app/lib/database/sqlite-no.go
@@ -1,6 +1,6 @@
 // Content managed by Project Forge, see [projectforge.md] for details.
-//go:build aix || android || dragonfly || freebsd || js || illumos || (linux && ppc64) || (linux && mips) || (linux && mipsle) || (linux && mips64) || (linux && mips64le) || (linux && ppc64) || (linux && ppc64le) || (linux && s390x) || netbsd || openbsd || plan9 || solaris || (windows && arm) || (windows && arm64)
-// +build aix android dragonfly freebsd js illumos linux,ppc64 linux,mips linux,mipsle linux,mips64 linux,mips64le linux,ppc64 linux,ppc64le linux,s390x netbsd openbsd plan9 solaris windows,arm windows,arm64
+//go:build aix || android || dragonfly || freebsd || js || illumos || (linux && ppc64) || (linux && mips) || (linux && mipsle) || (linux && mips64) || (linux && mips64le) || (linux && ppc64) || (linux && ppc64le) || (linux && s390x) || netbsd || openbsd || plan9 || solaris || (windows && 386) || (windows && arm) || (windows && arm64)
+// +build aix android dragonfly freebsd js illumos linux,ppc64 linux,mips linux,mipsle linux,mips64 linux,mips64le linux,ppc64 linux,ppc64le linux,s390x netbsd openbsd plan9 solaris windows,386 windows,arm windows,arm64
 
 package database
 
