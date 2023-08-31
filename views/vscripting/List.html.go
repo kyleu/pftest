d4fcc075e137a61ca4c6ad0b99bb10d8901ac37c
Kyle Unverferth <kyle@kyleu.com>
2023-08-31 12:41:49 -0600

    .

--- views/vscripting/List.html.go
+++ views/vscripting/List.html.go
@@ -10,97 +10,105 @@ package vscripting
 import (
 	"github.com/kyleu/pftest/app"
 	"github.com/kyleu/pftest/app/controller/cutil"
+	"github.com/kyleu/pftest/app/util"
 	"github.com/kyleu/pftest/views/components"
 	"github.com/kyleu/pftest/views/layout"
 )
 
-//line views/vscripting/List.html:9
+//line views/vscripting/List.html:10
 import (
 	qtio422016 "io"
 
 	qt422016 "github.com/valyala/quicktemplate"
 )
 
-//line views/vscripting/List.html:9
+//line views/vscripting/List.html:10
 var (
 	_ = qtio422016.Copy
 	_ = qt422016.AcquireByteBuffer
 )
 
-//line views/vscripting/List.html:9
+//line views/vscripting/List.html:10
 type List struct {
 	layout.Basic
 	Scripts []string
+	Sizes   map[string]int
 }
 
-//line views/vscripting/List.html:14
+//line views/vscripting/List.html:16
 func (p *List) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cutil.PageState) {
-//line views/vscripting/List.html:14
+//line views/vscripting/List.html:16
 	qw422016.N().S(`
   <div class="card">
     <div class="right"><a href="/admin/scripting/new"><button>New</button></a></div>
     <h3>`)
-//line views/vscripting/List.html:17
+//line views/vscripting/List.html:19
 	components.StreamSVGRefIcon(qw422016, `sitemap`, ps)
-//line views/vscripting/List.html:17
+//line views/vscripting/List.html:19
 	qw422016.N().S(` Scripts</h3>
     <div class="mt">
       <table class="min-200">
         <thead>
           <tr>
             <th class="shrink">Name</th>
+            <th class="text-align-right">Size</th>
           </tr>
         </thead>
         <tbody>
 `)
-//line views/vscripting/List.html:26
+//line views/vscripting/List.html:29
 	for _, x := range p.Scripts {
-//line views/vscripting/List.html:26
+//line views/vscripting/List.html:29
 		qw422016.N().S(`          <tr>
             <td><a href="/admin/scripting/`)
-//line views/vscripting/List.html:28
+//line views/vscripting/List.html:31
 		qw422016.E().S(x)
-//line views/vscripting/List.html:28
+//line views/vscripting/List.html:31
 		qw422016.N().S(`">`)
-//line views/vscripting/List.html:28
+//line views/vscripting/List.html:31
 		qw422016.E().S(x)
-//line views/vscripting/List.html:28
+//line views/vscripting/List.html:31
 		qw422016.N().S(`</a></td>
+            <td class="text-align-right">`)
+//line views/vscripting/List.html:32
+		qw422016.E().S(util.ByteSizeSI(int64(p.Sizes[x])))
+//line views/vscripting/List.html:32
+		qw422016.N().S(`</td>
           </tr>
 `)
-//line views/vscripting/List.html:30
+//line views/vscripting/List.html:34
 	}
-//line views/vscripting/List.html:30
+//line views/vscripting/List.html:34
 	qw422016.N().S(`        </tbody>
       </table>
     </div>
   </div>
 `)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 }
 
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 func (p *List) WriteBody(qq422016 qtio422016.Writer, as *app.State, ps *cutil.PageState) {
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	p.StreamBody(qw422016, as, ps)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	qt422016.ReleaseWriter(qw422016)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 }
 
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 func (p *List) Body(as *app.State, ps *cutil.PageState) string {
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	qb422016 := qt422016.AcquireByteBuffer()
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	p.WriteBody(qb422016, as, ps)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	qs422016 := string(qb422016.B)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	qt422016.ReleaseByteBuffer(qb422016)
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 	return qs422016
-//line views/vscripting/List.html:35
+//line views/vscripting/List.html:39
 }
