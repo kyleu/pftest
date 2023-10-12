a933b0e4402f0691296976f3f2d8ff7a6f1900f3
Kyle Unverferth <kyle@kyleu.com>
2023-10-11 18:32:32 -0600

    .

---
+++ bin/build/ci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Content managed by Project Forge, see [projectforge.md] for details.
+
+## Builds the app, installing all prerequisites
+
+set -euo pipefail
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir/../..
+
+echo "Installing utilities"
+./bin/bootstrap.sh
+
+echo "Compiling templates"
+./bin/templates.sh
+
+echo "Downloading dependencies"
+go mod download
+
+echo "Building client"
+./bin/build/client.sh
+
+echo "Building application"
+make build-release
