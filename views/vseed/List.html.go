14a39ff467e295c71cb20961c3d5a877a0b63e11
Kyle Unverferth <kyle@kyleu.com>
2023-11-23 13:13:20 -0700

    .

--- views/vseed/List.html.go
+++ views/vseed/List.html.go
@@ -41,7 +41,7 @@ func (p *List) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cutil.Pa
 //line views/vseed/List.html:17
 	qw422016.N().S(`
   <div class="card">
-    <div class="right"><a href="/seed/new"><button>New</button></a></div>
+    <div class="right"><a href="/seed/_new"><button>New</button></a></div>
     <h3>`)
 //line views/vseed/List.html:20
 	components.StreamSVGRefIcon(qw422016, `star`, ps)
