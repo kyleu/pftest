7c81a00463cd1eb9860903f4cd8ed456cb6c28e8
Kyle Unverferth <kyle@kyleu.com>
2024-03-02 10:36:28 -0700

    .

---
+++ app/util/markdown.go
@@ -0,0 +1,78 @@
+// Package util - Content managed by Project Forge, see [projectforge.md] for details.
+package util
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/samber/lo"
+)
+
+func MarkdownTable(header []string, rows [][]string, linebreak string) (string, error) {
+	maxes := lo.Map(header, func(h string, _ int) int {
+		return len(h)
+	})
+	for vi, v := range rows {
+		if len(v) != len(header) {
+			return "", errors.Errorf("row [%d] contains [%d] fields, but [%d] header fields were provided", vi, len(v), len(header))
+		}
+		lo.ForEach(v, func(cellVal string, cellIdx int) {
+			if cellIdx <= len(maxes)-1 && len(cellVal) > maxes[cellIdx] {
+				maxes[cellIdx] = len(cellVal)
+			}
+		})
+	}
+
+	ret := make([]string, 0, len(rows))
+	add := func(x []string) {
+		line := "| "
+		lo.ForEach(x, func(v string, vi int) {
+			mx := 0
+			if vi < len(maxes) {
+				mx = maxes[vi]
+			}
+			line += StringPad(v, mx)
+			if vi < len(x)-1 {
+				line += " | "
+			}
+		})
+		line += " |"
+		ret = append(ret, line)
+	}
+
+	add(header)
+
+	divider := "|-"
+	lo.ForEach(maxes, func(m int, mi int) {
+		lo.Times(m, func(_ int) struct{} {
+			divider += "-"
+			return struct{}{}
+		})
+		if mi < len(maxes)-1 {
+			divider += "-|-"
+		}
+	})
+	divider += "-|"
+	ret = append(ret, divider)
+
+	lo.ForEach(rows, func(row []string, _ int) {
+		add(row)
+	})
+	return strings.Join(ret, linebreak), nil
+}
+
+func MarkdownTableParse(md string) ([]string, [][]string) {
+	var header []string
+	var rows [][]string
+	for idx, line := range StringSplitLines(md) {
+		split := StringSplitAndTrim(line, "|")
+		if idx == 0 {
+			header = split
+		} else if idx == 1 {
+			// spacer, noop
+		} else {
+			rows = append(rows, split)
+		}
+	}
+	return header, rows
+}
