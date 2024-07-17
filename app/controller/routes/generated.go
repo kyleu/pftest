9b443a7d3097de264860c1294704990c7b9bffb9
Kyle Unverferth <kyle@kyleu.com>
2024-07-17 13:55:55 -0600

    .

--- app/controller/routes/generated.go
+++ app/controller/routes/generated.go
@@ -9,93 +9,150 @@ import (
 	"github.com/kyleu/pftest/app/controller/cg1/cg2"
 )
 
+const routeNew, routeRandom, routeEdit, routeDelete = "/_new", "/_random", "/edit", "/delete"
+
 func generatedRoutes(r *mux.Router) {
-	makeRoute(r, http.MethodGet, "/troub/le", controller.TroubleList)
-	makeRoute(r, http.MethodGet, "/troub/le/_new", controller.TroubleCreateForm)
-	makeRoute(r, http.MethodPost, "/troub/le/_new", controller.TroubleCreate)
-	makeRoute(r, http.MethodGet, "/troub/le/_random", controller.TroubleRandom)
-	makeRoute(r, http.MethodGet, "/troub/le/{from}/{where}", controller.TroubleDetail)
-	makeRoute(r, http.MethodGet, "/troub/le/{from}/{where}/edit", controller.TroubleEditForm)
-	makeRoute(r, http.MethodPost, "/troub/le/{from}/{where}/edit", controller.TroubleEdit)
-	makeRoute(r, http.MethodGet, "/troub/le/{from}/{where}/delete", controller.TroubleDelete)
-	makeRoute(r, http.MethodGet, "/timestamp", controller.TimestampList)
-	makeRoute(r, http.MethodGet, "/timestamp/_new", controller.TimestampCreateForm)
-	makeRoute(r, http.MethodPost, "/timestamp/_new", controller.TimestampCreate)
-	makeRoute(r, http.MethodGet, "/timestamp/_random", controller.TimestampRandom)
-	makeRoute(r, http.MethodGet, "/timestamp/{id}", controller.TimestampDetail)
-	makeRoute(r, http.MethodGet, "/timestamp/{id}/edit", controller.TimestampEditForm)
-	makeRoute(r, http.MethodPost, "/timestamp/{id}/edit", controller.TimestampEdit)
-	makeRoute(r, http.MethodGet, "/timestamp/{id}/delete", controller.TimestampDelete)
-	makeRoute(r, http.MethodGet, "/softdel", controller.SoftdelList)
-	makeRoute(r, http.MethodGet, "/softdel/_new", controller.SoftdelCreateForm)
-	makeRoute(r, http.MethodPost, "/softdel/_new", controller.SoftdelCreate)
-	makeRoute(r, http.MethodGet, "/softdel/_random", controller.SoftdelRandom)
-	makeRoute(r, http.MethodGet, "/softdel/{id}", controller.SoftdelDetail)
-	makeRoute(r, http.MethodGet, "/softdel/{id}/edit", controller.SoftdelEditForm)
-	makeRoute(r, http.MethodPost, "/softdel/{id}/edit", controller.SoftdelEdit)
-	makeRoute(r, http.MethodGet, "/softdel/{id}/delete", controller.SoftdelDelete)
-	makeRoute(r, http.MethodGet, "/seed", controller.SeedList)
-	makeRoute(r, http.MethodGet, "/seed/_new", controller.SeedCreateForm)
-	makeRoute(r, http.MethodPost, "/seed/_new", controller.SeedCreate)
-	makeRoute(r, http.MethodGet, "/seed/_random", controller.SeedRandom)
-	makeRoute(r, http.MethodGet, "/seed/{id}", controller.SeedDetail)
-	makeRoute(r, http.MethodGet, "/seed/{id}/edit", controller.SeedEditForm)
-	makeRoute(r, http.MethodPost, "/seed/{id}/edit", controller.SeedEdit)
-	makeRoute(r, http.MethodGet, "/seed/{id}/delete", controller.SeedDelete)
-	makeRoute(r, http.MethodGet, "/relation", controller.RelationList)
-	makeRoute(r, http.MethodGet, "/relation/_new", controller.RelationCreateForm)
-	makeRoute(r, http.MethodPost, "/relation/_new", controller.RelationCreate)
-	makeRoute(r, http.MethodGet, "/relation/_random", controller.RelationRandom)
-	makeRoute(r, http.MethodGet, "/relation/{id}", controller.RelationDetail)
-	makeRoute(r, http.MethodGet, "/relation/{id}/edit", controller.RelationEditForm)
-	makeRoute(r, http.MethodPost, "/relation/{id}/edit", controller.RelationEdit)
-	makeRoute(r, http.MethodGet, "/relation/{id}/delete", controller.RelationDelete)
-	makeRoute(r, http.MethodGet, "/reference", controller.ReferenceList)
-	makeRoute(r, http.MethodGet, "/reference/_new", controller.ReferenceCreateForm)
-	makeRoute(r, http.MethodPost, "/reference/_new", controller.ReferenceCreate)
-	makeRoute(r, http.MethodGet, "/reference/_random", controller.ReferenceRandom)
-	makeRoute(r, http.MethodGet, "/reference/{id}", controller.ReferenceDetail)
-	makeRoute(r, http.MethodGet, "/reference/{id}/edit", controller.ReferenceEditForm)
-	makeRoute(r, http.MethodPost, "/reference/{id}/edit", controller.ReferenceEdit)
-	makeRoute(r, http.MethodGet, "/reference/{id}/delete", controller.ReferenceDelete)
-	makeRoute(r, http.MethodGet, "/mixedcase", controller.MixedCaseList)
-	makeRoute(r, http.MethodGet, "/mixedcase/_new", controller.MixedCaseCreateForm)
-	makeRoute(r, http.MethodPost, "/mixedcase/_new", controller.MixedCaseCreate)
-	makeRoute(r, http.MethodGet, "/mixedcase/_random", controller.MixedCaseRandom)
-	makeRoute(r, http.MethodGet, "/mixedcase/{id}", controller.MixedCaseDetail)
-	makeRoute(r, http.MethodGet, "/mixedcase/{id}/edit", controller.MixedCaseEditForm)
-	makeRoute(r, http.MethodPost, "/mixedcase/{id}/edit", controller.MixedCaseEdit)
-	makeRoute(r, http.MethodGet, "/mixedcase/{id}/delete", controller.MixedCaseDelete)
-	makeRoute(r, http.MethodGet, "/g1/g2/path", cg2.PathList)
-	makeRoute(r, http.MethodGet, "/g1/g2/path/_new", cg2.PathCreateForm)
-	makeRoute(r, http.MethodPost, "/g1/g2/path/_new", cg2.PathCreate)
-	makeRoute(r, http.MethodGet, "/g1/g2/path/_random", cg2.PathRandom)
-	makeRoute(r, http.MethodGet, "/g1/g2/path/{id}", cg2.PathDetail)
-	makeRoute(r, http.MethodGet, "/g1/g2/path/{id}/edit", cg2.PathEditForm)
-	makeRoute(r, http.MethodPost, "/g1/g2/path/{id}/edit", cg2.PathEdit)
-	makeRoute(r, http.MethodGet, "/g1/g2/path/{id}/delete", cg2.PathDelete)
-	makeRoute(r, http.MethodGet, "/capital", controller.CapitalList)
-	makeRoute(r, http.MethodGet, "/capital/_new", controller.CapitalCreateForm)
-	makeRoute(r, http.MethodPost, "/capital/_new", controller.CapitalCreate)
-	makeRoute(r, http.MethodGet, "/capital/_random", controller.CapitalRandom)
-	makeRoute(r, http.MethodGet, "/capital/{id}", controller.CapitalDetail)
-	makeRoute(r, http.MethodGet, "/capital/{id}/edit", controller.CapitalEditForm)
-	makeRoute(r, http.MethodPost, "/capital/{id}/edit", controller.CapitalEdit)
-	makeRoute(r, http.MethodGet, "/capital/{id}/delete", controller.CapitalDelete)
-	makeRoute(r, http.MethodGet, "/basic", controller.BasicList)
-	makeRoute(r, http.MethodGet, "/basic/_new", controller.BasicCreateForm)
-	makeRoute(r, http.MethodPost, "/basic/_new", controller.BasicCreate)
-	makeRoute(r, http.MethodGet, "/basic/_random", controller.BasicRandom)
-	makeRoute(r, http.MethodGet, "/basic/{id}", controller.BasicDetail)
-	makeRoute(r, http.MethodGet, "/basic/{id}/edit", controller.BasicEditForm)
-	makeRoute(r, http.MethodPost, "/basic/{id}/edit", controller.BasicEdit)
-	makeRoute(r, http.MethodGet, "/basic/{id}/delete", controller.BasicDelete)
-	makeRoute(r, http.MethodGet, "/audited", controller.AuditedList)
-	makeRoute(r, http.MethodGet, "/audited/_new", controller.AuditedCreateForm)
-	makeRoute(r, http.MethodPost, "/audited/_new", controller.AuditedCreate)
-	makeRoute(r, http.MethodGet, "/audited/_random", controller.AuditedRandom)
-	makeRoute(r, http.MethodGet, "/audited/{id}", controller.AuditedDetail)
-	makeRoute(r, http.MethodGet, "/audited/{id}/edit", controller.AuditedEditForm)
-	makeRoute(r, http.MethodPost, "/audited/{id}/edit", controller.AuditedEdit)
-	makeRoute(r, http.MethodGet, "/audited/{id}/delete", controller.AuditedDelete)
+	generatedRoutesTrouble(r, "/troub/le")
+	generatedRoutesTimestamp(r, "/timestamp")
+	generatedRoutesSoftdel(r, "/softdel")
+	generatedRoutesSeed(r, "/seed")
+	generatedRoutesRelation(r, "/relation")
+	generatedRoutesReference(r, "/reference")
+	generatedRoutesMixedCase(r, "/mixedcase")
+	generatedRoutesPath(r, "/g1/g2/path")
+	generatedRoutesCapital(r, "/capital")
+	generatedRoutesBasic(r, "/basic")
+	generatedRoutesAudited(r, "/audited")
+}
+
+func generatedRoutesTrouble(r *mux.Router, prefix string) {
+	const pkn = "/{from}/{where}"
+	makeRoute(r, http.MethodGet, prefix, controller.TroubleList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.TroubleCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.TroubleCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.TroubleRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.TroubleDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.TroubleEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.TroubleEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.TroubleDelete)
+}
+
+func generatedRoutesTimestamp(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.TimestampList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.TimestampCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.TimestampCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.TimestampRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.TimestampDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.TimestampEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.TimestampEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.TimestampDelete)
+}
+
+func generatedRoutesSoftdel(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.SoftdelList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.SoftdelCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.SoftdelCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.SoftdelRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.SoftdelDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.SoftdelEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.SoftdelEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.SoftdelDelete)
+}
+
+func generatedRoutesSeed(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.SeedList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.SeedCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.SeedCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.SeedRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.SeedDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.SeedEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.SeedEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.SeedDelete)
+}
+
+func generatedRoutesRelation(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.RelationList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.RelationCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.RelationCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.RelationRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.RelationDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.RelationEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.RelationEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.RelationDelete)
+}
+
+func generatedRoutesReference(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.ReferenceList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.ReferenceCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.ReferenceCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.ReferenceRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.ReferenceDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.ReferenceEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.ReferenceEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.ReferenceDelete)
+}
+
+func generatedRoutesMixedCase(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.MixedCaseList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.MixedCaseCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.MixedCaseCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.MixedCaseRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.MixedCaseDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.MixedCaseEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.MixedCaseEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.MixedCaseDelete)
+}
+
+func generatedRoutesPath(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, cg2.PathList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, cg2.PathCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, cg2.PathCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, cg2.PathRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, cg2.PathDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, cg2.PathEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, cg2.PathEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, cg2.PathDelete)
+}
+
+func generatedRoutesCapital(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.CapitalList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.CapitalCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.CapitalCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.CapitalRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.CapitalDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.CapitalEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.CapitalEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.CapitalDelete)
+}
+
+func generatedRoutesBasic(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.BasicList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.BasicCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.BasicCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.BasicRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.BasicDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.BasicEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.BasicEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.BasicDelete)
+}
+
+func generatedRoutesAudited(r *mux.Router, prefix string) {
+	const pkn = "/{id}"
+	makeRoute(r, http.MethodGet, prefix, controller.AuditedList)
+	makeRoute(r, http.MethodGet, prefix+routeNew, controller.AuditedCreateForm)
+	makeRoute(r, http.MethodPost, prefix+routeNew, controller.AuditedCreate)
+	makeRoute(r, http.MethodGet, prefix+routeRandom, controller.AuditedRandom)
+	makeRoute(r, http.MethodGet, prefix+pkn, controller.AuditedDetail)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeEdit, controller.AuditedEditForm)
+	makeRoute(r, http.MethodPost, prefix+pkn+routeEdit, controller.AuditedEdit)
+	makeRoute(r, http.MethodGet, prefix+pkn+routeDelete, controller.AuditedDelete)
 }
