93db687d5b5f895713fd0a92f8e45a431e6e92e4
Kyle Unverferth <kyle@kyleu.com>
2024-09-15 11:02:50 -0600

    .

--- views/vtrouble/Detail.html.go
+++ views/vtrouble/Detail.html.go
@@ -40,20 +40,20 @@ func (p *Detail) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cutil.
 	qw422016.N().S(`
   <div class="card">
     <div class="right">
-      <a href="#modal-trouble"><button type="button">`)
+      <a href="#modal-trouble"><button type="button" title="JSON">`)
 //line views/vtrouble/Detail.html:19
-	components.StreamSVGButton(qw422016, "file", ps)
+	components.StreamSVGButton(qw422016, "code", ps)
 //line views/vtrouble/Detail.html:19
-	qw422016.N().S(` JSON</button></a>
+	qw422016.N().S(`</button></a>
       <a href="`)
 //line views/vtrouble/Detail.html:20
 	qw422016.E().S(p.Model.WebPath(p.Paths...))
 //line views/vtrouble/Detail.html:20
-	qw422016.N().S(`/edit"><button>`)
+	qw422016.N().S(`/edit" title="Edit"><button>`)
 //line views/vtrouble/Detail.html:20
 	components.StreamSVGButton(qw422016, "edit", ps)
 //line views/vtrouble/Detail.html:20
-	qw422016.N().S(` Edit</button></a>
+	qw422016.N().S(`</button></a>
     </div>
     <h3>`)
 //line views/vtrouble/Detail.html:22
